e34999f7ced411321715f4a41dd5776962aa1701
artemiev <mixartemev@gmail.com>
2021-02-17 00:45:55 +0300

    prepare data

---
+++ main.py
@@ -0,0 +1,25 @@
+# Load Dependencies
+import numpy as np
+import pandas_datareader as web
+import datetime as dt
+from sklearn.preprocessing import MinMaxScaler
+
+# Load Data
+company = 'APPL'
+start = dt.datetime(2015, 1, 1)
+end = dt.datetime(2021, 1, 1)
+data = web.DataReader(company, 'yahoo', start, end)
+
+# Prepare Data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+prediction_days = 60
+x_train = []
+y_train = []
+
+for x in range(prediction_days, len(scaled_data)):
+    x_train.append(scaled_data[x-prediction_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
