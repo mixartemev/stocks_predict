bd26b066ee62a859d9241ba893e7a5dbedd25558
artemiev <mixartemev@gmail.com>
2021-02-18 01:26:24 +0300

    build model

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # Load Dependencies
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 import pandas_datareader as web
 import datetime as dt
 from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, LSTM
 
 # Load Data
 company = 'IBM'
-start = dt.datetime(2015, 1, 1)
+start = dt.datetime(2019, 1, 1)
 end = dt.datetime(2021, 1, 1)
 data = web.DataReader(company, 'yahoo', start, end)  # .reset_index()
 
@@ -23,3 +27,17 @@ for x in range(prediction_days, len(scaled_data)):
 
 x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+# Build Model
+model = Sequential()
+
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))  # Prediction of next close price
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+model.fit(x_train, y_train, epochs=25, batch_size=32)
