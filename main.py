5b504f39cab7fb05e0a7637d0aba83244955635f
artemiev <mixartemev@gmail.com>
2021-02-26 17:31:30 +0300

    simplicity

--- main.py
+++ main.py
@@ -7,30 +7,38 @@ from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
+# Init vars
+company = 'TSLA'
+start_train = dt.date(2015, 1, 1)
+end = dt.date.today()
+pred_days = 50
+test_days = 250
+
 # Load Data
-company = 'AAPL'
-start = dt.datetime(2021, 1, 15)
-end = dt.datetime(2021, 2, 10)
-train_prices_col = web.DataReader(company, 'yahoo', start, end)['Close'].values.reshape(-1, 1)
+prices_col = web.DataReader(company, 'yahoo', start_train, end)['Close'].values.reshape(-1, 1)
 
 # Prepare Data
 scaler = MinMaxScaler(feature_range=(0, 1))
-train_data_col = scaler.fit_transform(train_prices_col)
-prediction_days = 5
-x_train_matrix = []
-y_train_row = []
+data_col = scaler.fit_transform(prices_col)
 
-for x in range(prediction_days, len(train_data_col)):
-    x_train_matrix.append(train_data_col[x - prediction_days:x, 0])  # input
-    y_train_row.append(train_data_col[x, 0])  # output
+x_matrix = []
+y_row = []
+for i in range(pred_days, len(data_col)):
+    x_matrix.append(data_col[i-pred_days:i, 0])  # input
+    y_row.append(data_col[i, 0])  # output
 
-x_train_matrix, y_train_row = np.array(x_train_matrix), np.array(y_train_row)
-x_train_matrix = x_train_matrix.reshape((x_train_matrix.shape[0], x_train_matrix.shape[1], 1))
+x_matrix, y_row = np.array(x_matrix), np.array(y_row)
+x_matrix = x_matrix.reshape((x_matrix.shape[0], x_matrix.shape[1], 1))
+# tmp only for debug
+x_train_matrix = x_matrix[:-test_days]
+x_test_matrix = x_matrix[-test_days:]
+y_train_row = y_row[:-test_days]
+y_test_row = y_row[-test_days:]
 
 # Build Model
 model = Sequential()
 
-model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train_matrix.shape[1], 1)))
+model.add(LSTM(units=50, return_sequences=True, input_shape=(pred_days, 1)))
 model.add(Dropout(0.2))
 model.add(LSTM(units=50, return_sequences=True))
 model.add(Dropout(0.2))
@@ -39,43 +47,25 @@ model.add(Dropout(0.2))
 model.add(Dense(units=1))  # Prediction of next close price
 
 model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train_matrix, y_train_row, epochs=5, batch_size=32)
-
-# Test the model accuracy on existed data
-test_start = dt.datetime(2021, 2, 11)
-test_end = dt.datetime.now()
-
-actual_prices_col = web.DataReader(company, 'yahoo', test_start, test_end)['Close'].values.reshape(-1, 1)
-
-model_inputs = np.vstack((train_prices_col[-prediction_days:], actual_prices_col))
-model_inputs = scaler.transform(model_inputs)
+model.fit(x_train_matrix, y_train_row, epochs=25, batch_size=32)
 
 # Make predictions on test data
-x_test = []
-
-for x in range(prediction_days, len(model_inputs)):
-    x_test.append(model_inputs[x - prediction_days: x, 0])
-
-x_test = np.array(x_test)
-x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
-
-predicted_data_col = model.predict(x_test)
+predicted_data_col = model.predict(x_test_matrix)
 predicted_prices_col = scaler.inverse_transform(predicted_data_col)
 
+# Predict tomorrow
+real_data = np.array([y_test_row[-pred_days:]])
+real_data = real_data.reshape((1, pred_days, 1))
+
+prediction = model.predict(real_data)
+prediction = scaler.inverse_transform(prediction)[0][0]
+print(f"Tomorrow {company} price: {prediction}")
+
 # Plot predicted and actual prices
-plt.plot(actual_prices_col, color='black', label=f'Actual {company} price')
+plt.plot(prices_col[-test_days:], color='black', label=f'Actual {company} price')
 plt.plot(predicted_prices_col, color='green', label=f'Predicted {company} price')
-plt.title(f'{company} Share Price')
+plt.title(f'Tomorrow {company} Price: ${prediction:.2f}')
 plt.xlabel('Time')
 plt.xlabel('Price')
 plt.legend()
 plt.show()
-
-# Predict tomorrow
-real_data = [model_inputs[len(model_inputs) - prediction_days: len(model_inputs), 0]]
-real_data = np.array(real_data)
-real_data = np.reshape(real_data, (real_data.shape[0], real_data.shape[1], 1))
-
-prediction = model.predict(real_data)
-prediction = scaler.inverse_transform(prediction)
-print(f"Tomorrow {company} price: {prediction[0][0]}")
